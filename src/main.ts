e3f1d3b2a94975ce187ea6bbc8e65aa0fe8e17eb
Patricia Almeida <patricia.almeida.dev@gmail.com>
2025-03-16 23:29:18 +0000

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://resutune-taupe.vercel.app/", // adjust as needed or use a wildcard during development
+    origin: "https://resutune-taupe.vercel.app", // adjust as needed or use a wildcard during development
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3100);
